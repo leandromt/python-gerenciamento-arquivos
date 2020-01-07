6df9ac6a0ce48fcc90f7c2f91f703615c42b13ab
Leandro Tavares <leandro.mendestavares@gmail.com>
2020-01-06 23:04:43 -0300

    File open

--- file.py
+++ file.py
@@ -1,2 +1,22 @@
-from test import teste_file
-teste_file.hello_word('Leandro Tavares')
+# from test import teste_file
+# teste_file.hello_word('Leandro Tavares')
+
+import os
+
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+print(BASE_PATH)
+
+# OPEN - funcao para abrir um arquivo existente
+# 1 - passar o caminho do arquivo
+# 2 - modo ( w - write, r - reader)
+# file = open(BASE_PATH + '/_file_test.dat', 'w')
+file = open(BASE_PATH + '/_file_test.dat', 'a')
+# https://docs.python.org/3/library/functions.html?highlight=open#open
+file.write('Scholl of net')
+file.write('\n')
+file.write('Teste 2')
+file.write('\n')
+file.write('teste 4')
+file.writelines(('aaaaaaaaa','bbbbbbbb','cccccccc'))
+file.writelines(['ddddddddd', '\n', 'eeeeeeee', '\n', 'ffffffff'])
+file.close()
